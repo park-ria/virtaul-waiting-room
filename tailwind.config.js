1451f38aff05bf5063aebbbd5b82e6d9c8108fef
박리아 <danny6217@gmail.com>
2025-08-28 21:22:36 +0900

    tailwindcss 설치

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{ts,tsx}", // App Router 경로
+    "./src/components/**/*.{ts,tsx}", // 컴포넌트 경로 (필요하면)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
